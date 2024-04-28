8c4b68a7df74f07c0b35fb54fcf20a0dac96120e
abdsarmai <88580232+abdsarmai@users.noreply.github.com>
2024-04-28 19:29:58 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
-apt-get install gnupg apt-transport-https
-curl -s https://packages.wazuh.com/key/GPG-KEY-WAZUH | gpg --no-default-keyring --keyring gnupg-ring:/usr/share/keyrings/wazuh.gpg --import && chmod 644 /usr/share/keyrings/wazuh.gpg
-echo "deb [signed-by=/usr/share/keyrings/wazuh.gpg] https://packages.wazuh.com/4.x/apt/ stable main" | tee -a /etc/apt/sources.list.d/wazuh.list
-apt-get -y install wazuh-manager
-systemctl daemon-reload
-systemctl enable wazuh-manager
-systemctl start wazuh-manager
-systemctl status wazuh-manager
-apt-get -y install filebeat
-curl -so /etc/filebeat/filebeat.yml https://packages.wazuh.com/4.7/tpl/wazuh/filebeat/filebeat.yml
+sudo apt update && sudo apt install curl apt-transport-https unzip wget libcap2-bin software-properties-common lsb-release gnupg2
+curl -sO https://packages.wazuh.com/4.5/wazuh-install.sh && chmod 744 wazuh-install.sh && bash ./wazuh-install.sh -a
 
